8a1002c1fb5a36fc34e51e6c270002da3206a97d
Bùi Văn Đức <84630787+buiduc111398@users.noreply.github.com>
2023-05-21 23:16:43 +0700

    app.js
    Signed-off-by: Bùi Văn Đức <84630787+buiduc111398@users.noreply.github.com>

--- app.js
+++ app.js
@@ -11,3 +11,45 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+function updateSubtotal(input) {
+    var row = input.parentNode.parentNode; // Lấy thẻ cha (row) của trường input
+    var priceCell = row.cells[3]; // Ô chứa giá sản phẩm
+    var subtotalCell = row.cells[5]; // Ô chứa tổng giá trị
+
+    var price = parseFloat(priceCell.innerText.replace('$', '')); // Lấy giá sản phẩm và chuyển sang số thực
+    var quantity = parseInt(input.value); // Lấy số lượng sản phẩm
+    if (quantity < 0) {
+        input.value = 0;
+        quantity = 0;
+    }
+    var subtotal = price * quantity; // Tính toán tổng giá trị
+
+    subtotalCell.innerText = '$' + subtotal.toFixed(2); // Cập nhật tổng giá trị mới
+
+    calculateTotals();
+}
+function calculateTotals() {
+    var cartSubtotal = 0;
+    var subtotalCells = document.querySelectorAll("#cart tbody tr td:nth-child(6)");
+    for (var i = 0; i < subtotalCells.length; i++) {
+        var subtotalText = subtotalCells[i].innerText;
+        var subtotalValue = parseFloat(subtotalText.replace('$', ''));
+        cartSubtotal += subtotalValue;
+    }
+
+    var cartSubtotalCell = document.querySelector("#subtotal table tr:first-child td:last-child");
+    cartSubtotalCell.innerText = "$ " + cartSubtotal.toFixed(2);
+
+    var totalAmountCell = document.querySelector("#subtotal table tr:last-child td:last-child");
+    totalAmountCell.innerText = "$ " + cartSubtotal.toFixed(2);
+}
+
+// Lắng nghe sự kiện nhập input
+var quantityInputs = document.querySelectorAll("#cart tbody tr td:nth-child(5) input[type='number']");
+for (var i = 0; i < quantityInputs.length; i++) {
+    quantityInputs[i].addEventListener('input', function () {
+        updateSubtotal(this);
+    });
+}
+
+calculateTotals();
